3458ff1dd322f36c66b9fd3111d4f2e37b106613
Rodrigo Baeza <94202438+Electro-Kerubin@users.noreply.github.com>
2026-09-05 12:14:39 -0400

    fix: eliminar EnableDiscoveryClient ultimos rastros tras remover dependencia de Eureka

--- src/main/java/org/sanosysalvos/GeolocationServiceApplication.java
+++ src/main/java/org/sanosysalvos/GeolocationServiceApplication.java
@@ -2,7 +2,6 @@ package org.sanosysalvos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class GeolocationServiceApplication {
